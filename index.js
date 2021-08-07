24b865db3f6810deeb2f923336c3660e2cd8eaa0
Alim Sadetov <sadetovalim@gmail.com>
2021-08-07 16:11:54 +0300

    third

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app     = express();
 
 const TOKEN = '1926731720:AAE-mYixPbnnhLRGU0NqDwvs_4AL0XNACqg';
 
-let URL = 'https://www.avito.ru/respublika_krym/bytovaya_elektronika?s=104';
+let URL = 'https://www.avito.ru/simferopol/tovary_dlya_kompyutera?f=ASgBAgECAUT4vA2Y0jQBRcaaDBV7ImZyb20iOjAsInRvIjoyMDAwMH0&geoCoords=44.948314%2C34.100192&localPriority=1&radius=25&s=104&user=1';
+let URL2 = 'https://www.avito.ru/simferopol/nastolnye_kompyutery?geoCoords=44.948314%2C34.100192&pmax=30000&radius=25&s=104&user=1';
 
 const bot = new TelegramBot(TOKEN, {
     polling: true
@@ -42,6 +43,16 @@ bot.on('text', (msg) =>{
             }
             
         });
+        needle.get(URL2, function(err, res){
+          if (err) throw err;
+          //console.log(res.body);
+          const $ = cheerio.load(res.body);
+          if ((`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}` !== result) && (`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}` !== 'https://www.avito.ruundefined')){
+              bot.sendMessage(msg.chat.id, `https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`);
+              result = `https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`
+          }
+          
+      });
     }, 10000);
     }
     else{
