f4a077774e07dd30d27a5c05bca718aa8ad1879e
Alim Sadetov <sadetovalim@gmail.com>
2021-08-15 17:46:27 +0300

    fix

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const app     = express();
 
+//`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>.iva-item-content-m2FiN>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`)
+
 const TOKEN = '1926731720:AAE-mYixPbnnhLRGU0NqDwvs_4AL0XNACqg';
 
 let URL = 'https://www.avito.ru/simferopol/tovary_dlya_kompyutera?f=ASgBAgECAUT4vA2Y0jQBRcaaDBV7ImZyb20iOjAsInRvIjoyMDAwMH0&geoCoords=44.948314%2C34.100192&localPriority=1&radius=25&s=104&user=1';
 let URL2 = 'https://www.avito.ru/simferopol/nastolnye_kompyutery?geoCoords=44.948314%2C34.100192&pmax=30000&radius=25&s=104&user=1';
-
+//let URL = 'https://www.avito.ru/simferopol/tovary_dlya_kompyutera?f=ASgBAgECAUT4vA2Y0jQBRcaaDBV7ImZyb20iOjAsInRvIjoyMDAwMH0&s=104&user=1'
 const bot = new TelegramBot(TOKEN, {
     polling: true
   });
@@ -24,6 +26,7 @@ app.get('/', function(request, response) {
   });
 
 let result = '';
+let result2 ='';
 
 
 
@@ -34,22 +37,24 @@ bot.on('text', (msg) =>{
       bot.sendMessage(msg.chat.id, `Привет, ${msg.chat.first_name}, сейчас пойдёт куча ссылок с обЪявлениями. Бот проверяет авито каждые 10 секунд.`);
       setInterval(function () {
         needle.get(URL, function(err, res){
-            if (err) throw err;
             //console.log(res.body);
-            const $ = cheerio.load(res.body);
-            if ((`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}` !== result) && (`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}` !== 'https://www.avito.ruundefined')){
-                bot.sendMessage(msg.chat.id, `https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`);
-                result = `https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`
+            if (err) return;
+            let $ = cheerio.load(res.body);
+            console.log(`https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}`)
+            if ((`https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}` !== result) && (`https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}` !== 'https://www.avito.ruundefined')){
+                bot.sendMessage(msg.chat.id, `https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}`);
+                result = `https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}`
             }
             
         });
-        needle.get(URL2, function(err, res){
-          if (err) throw err;
+        needle.get(URL2, function(err2, res2){
           //console.log(res.body);
-          const $ = cheerio.load(res.body);
-          if ((`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}` !== result) && (`https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}` !== 'https://www.avito.ruundefined')){
-              bot.sendMessage(msg.chat.id, `https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`);
-              result = `https://www.avito.ru${$('div[data-marker = catalog-serp]>div[data-marker = item]>div>.iva-item-body-NPl6W>.iva-item-titleStep-2bjuh>a').attr("href")}`
+          if (err2) return;
+          let $ = cheerio.load(res2.body);
+          console.log(`https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}`)
+          if ((`https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}` !== result2) && (`https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}` !== 'https://www.avito.ruundefined')){
+              bot.sendMessage(msg.chat.id, `https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}`);
+              result2 = `https://www.avito.ru/${$('div[data-marker = catalog-serp]>div[data-marker = item]').attr("data-item-id")}`
           }
           
       });
